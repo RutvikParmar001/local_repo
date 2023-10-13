a6391bbe4b84290b90cb70888f38569131fdabde
Rutvik Parmar <parmarrutvik1414@gmail.com>
2023-10-13 20:12:12 +0530

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+
+   res.sendFile(__dirname,'/public/index.html');
+
+})
+
+
+app.get("/about",(req,res)=>{
+
+    res.send("Hello you shuold know me");
+
+})
+
+
+
+app.listen(port,()=>{
+    console.log("This is done");
+})
