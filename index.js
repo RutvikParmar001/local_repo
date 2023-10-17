0f0818f2ee0693ae4e1951ef655b23981afb4636
Rutvik Parmar <parmarrutvik1414@gmail.com>
2023-10-17 22:39:51 +0530

    add new

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
 const express = require("express");
-const path = require("path");
 const hbs = require("hbs");
+const path = require("path");
+
+const port = 5000;
+
 const app = express();
 
-
-const newpath = path.join(__dirname,"/templats/views");
-const partialspath = path.join(__dirname,"/templats/partials")
-
+const newpath = path.join(__dirname,'/templates/partials');
+const new_path = path.join(__dirname,'/templates/views');
 
 
-const port = 3000;
 
-//? middleware
-
-// app.use(express.static('public'));
 app.set('view engine','hbs');
-app.set('views',newpath);
-hbs.registerPartials(partialspath);
+app.set('views',new_path);
+hbs.registerPartials(newpath);
 
 
 app.get("/",(req,res)=>{
 
-//? for handlbar
-
-res.render('index');
-
-// res.send("hello how are you");
-
-
-//? for html file
-//    res.sendFile(__dirname,'/public/index.html');
-
-})
-
-app.get("/weather",(req,res)=>{
-
-   res.render("weather");
+    res.render('index');
 
 })
 
 
 app.listen(port,()=>{
-    console.log("This is done");
+    console.log("this is done bro");
 })
+
+
+
+
+
+
+// const express = require("express");
+// const path = require("path");
+// const hbs = require("hbs");
+// const app = express();
+
+
+// const newpath = path.join(__dirname,"/templats/views");
+// const partialspath = path.join(__dirname,"/templats/partials")
+
+
+
+// const port = 3000;
+
+// //? middleware
+
+// // app.use(express.static('public'));
+// app.set('view engine','hbs');
+// app.set('views',newpath);
+// hbs.registerPartials(partialspath);
+
+
+// app.get("/",(req,res)=>{
+
+// //? for handlbar
+
+// res.render('index');
+
+// // res.send("hello how are you");
+
+
+// //? for html file
+// //    res.sendFile(__dirname,'/public/index.html');
+
+// })
+
+// app.get("/weather",(req,res)=>{
+
+//    res.render("weather");
+
+// })
+
+
+// app.listen(port,()=>{
+//     console.log("This is done");
+// })
